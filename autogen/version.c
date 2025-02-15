5dcfa0459fc8b4c93c049aa8c76ea1f6552ccd7a
Jack Yang <jack.v.yang@gmail.com>
2025-02-15 18:54:53 +1300

    del autogen

--- autogen/version.c
+++
@@ -1,4 +0,0 @@
-const unsigned char fw_version_major_ = 0;
-const unsigned char fw_version_minor_ = 5;
-const unsigned char fw_version_revision_ = 6;
-const unsigned char fw_version_unreleased_ = 1;
